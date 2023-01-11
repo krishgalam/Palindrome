d19e65f42dd3fcd408ab3eca3bdc29eb0a64b4a1
krishgalam <krishgalam@gmail.com>
2023-01-11 10:39:07 +0530

    updated logic

--- Palindrome.cs
+++ Palindrome.cs
@@ -2,39 +2,31 @@ public static class Palindrome
 {
     public static void CheckIfPalinDrome(int paramValue)
     {
-        // Validation
-
         // Check basic length if lenght is even not a palindrome
         int paramLength = paramValue.ToString().Length;
         if (paramLength == 1)
         {
-            Console.WriteLine("Success: Given value is a Palindrome");
+            Console.WriteLine("Success: Given value " + paramValue + " is a Palindrome");
             return;
         }
         int splitValueIndex = 0;
-        Console.WriteLine("Halflength" + (paramLength) / 2);
         splitValueIndex = paramLength % 2 == 0 ? (paramLength) / 2 : (paramLength - 1) / 2;
-        Console.WriteLine("splitValueIndex " + splitValueIndex);
-        Console.WriteLine(splitValueIndex);
         int counter = paramLength - 1;
         var paramValueCharArray = paramValue.ToString().ToCharArray();
         for (int i = 0; i < splitValueIndex; i++)
         {
-            Console.WriteLine("FirstHalf " + paramValueCharArray[i]);
-            Console.WriteLine("SecondHalf " + paramValueCharArray[counter]);
-
             if (paramValueCharArray[i] == paramValueCharArray[counter])
             {
                 counter--;
                 if (counter <= 1)
                 {
-                    Console.WriteLine("Success:"+ paramValue +" Given value is a Palindrome");
+                    Console.WriteLine("Success: Given value " + paramValue + " is a Palindrome");
                 }
 
             }
             else
             {
-                Console.WriteLine("Invalid:" + paramValue + "Given value is not a Palindrome");
+                Console.WriteLine("Invalid: Given value " + paramValue + " is not a Palindrome");
             }
         }
     }
